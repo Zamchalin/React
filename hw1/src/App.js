ae539a07977f346d64aee9429151d361f4a99362
Michael <zamchalin@mail.ru>
2024-10-16 22:56:00 +0300

    added hw1

--- hw1/src/App.js
+++ hw1/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Message from "./Message";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message text="Это первый пропс" />
+      <Message text="Это второй пропс" />
+      <Message text="Это третий пропс" />
+      <Message text="Это четвёртый пропс" />
+      <Message text="Это пятый пропс" />
     </div>
   );
 }
---
+++ hw1/src/Message.js
@@ -0,0 +1,9 @@
+function Message(props) {
+  return (
+    <div className="props-text_contain">
+      <p className="props-text">{props.text}</p>
+    </div>
+  );
+}
+
+export default Message;
